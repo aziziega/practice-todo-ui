9e68ff4007f3857756a4e61217eac602304c3935
Wildan Nur Rahman <wildannurrahman30@gmail.com>
2025-02-09 22:01:12 +0700

    patch: update contract address

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,5 +2,5 @@ import TodoListAbi from '../contracts/TodoList.json';
 
 export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
 export const TODOLIST_ABI = TodoListAbi;
-export const TODOLIST_ADDRESS = '0xffcC9e497CbbED822C8Fa4b54bbB41321993231E';
+export const TODOLIST_ADDRESS = '0xdf6f19AA5987fE944AD6d7FC49B0ab5fD5978717';
 
